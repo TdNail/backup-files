a11e2c4f5cdefeeb689dc6b7af9e5fd29a7c77a5
Nailson Vilela <33153302+TdNail@users.noreply.github.com>
2023-07-27 08:07:29 -0300

    Update backupFiles.py

--- backupFiles.py
+++ backupFiles.py
@@ -4,26 +4,26 @@ import datetime
 
 
 def cleanup_old_backups(destination_dir, max_backups):
-    # Obter a lista de backups existentes no diretório de destino
+    # Coleta uma lista com os backups existentes no diretório.
     backup_folders = [f for f in os.listdir(destination_dir) if f.startswith("Backup_")]
     backup_folders.sort()
 
-    # Verificar se há mais de max_backups backups, se sim, remover o primeiro backup realizado
+    # Verifica se excedeu o máximo de backup e exclui o primeiro que foi criado.
     if len(backup_folders) >= max_backups:
         oldest_backup = os.path.join(destination_dir, backup_folders[0])
         shutil.rmtree(oldest_backup)
 
 def backup_files(source_dir, destination_dir, log_file, log_dir, max_backups=5):
     try:
-        # Verificar se o diretório de destino existe, caso não, criar.
+        # Verifica se o diretório existe, se não, ele cria um novo.
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir)
             os.makedirs(log_dir)
 
-        # Limpar backups antigos, se houver mais de max_backups
+        # Chama a função que verifica o quantidade de backup existentes e apagar se for necessário, neste caso o máximo é 5.
         cleanup_old_backups(destination_dir, max_backups)
 
-        # Gera o nome do diretório de backup com data e hora
+        # Cria o nome do diretório de backup com data e hora
         log_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         backup_dir_name = f"Backup_{log_time}"
 
@@ -32,14 +32,14 @@ def backup_files(source_dir, destination_dir, log_file, log_dir, max_backups=5):
             log_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             backup_dir_name = f"Backup_{log_time}"
 
-        # Criar o caminho completo para o diretório de backup
+        # Cria o caminho completo para o diretório de backup
         backup_dir_path = os.path.join(destination_dir, backup_dir_name)
         backup_dirLog_path = os.path.join(log_dir)
 
-        # Copiar os arquivos do diretório de origem para o diretório de backup
+        # Realiza a copia os arquivos do diretório de origem para o diretório o destino
         shutil.copytree(source_dir, backup_dir_path)
 
-        # Escrever o log com a data e hora em que o backup foi realizado
+        # Cria um log com a data e hora em que o backup foi realizado e apenas edita caso seja realizado mais vezes
         log_path = os.path.join(backup_dirLog_path, f"{log_file}.txt")
         with open(log_path, "a") as log_file:
             log_file.write(f"Backup realizado em: {log_time}\n")
@@ -48,7 +48,7 @@ def backup_files(source_dir, destination_dir, log_file, log_dir, max_backups=5):
     except Exception as e:
         print(f"Erro ao realizar o backup: {e}")
 
-# Exemplo de uso:
+
 if __name__ == "__main__":
     source_directory = r"C:\Users\Nailson\Desktop\Backup teste python"
     destination_directory = r"C:\Users\Nailson\Desktop\Destino backup teste"
